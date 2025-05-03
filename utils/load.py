f66084c8a4d3e628a70dd4cd7c80fc43f1ea6b92
juanvieira <juanvieira1@yahoo.com>
2025-05-02 22:10:38 -0300

    feat: add module to load stuffs

---
+++ utils/load.py
@@ -0,0 +1,14 @@
+from langchain_community.document_loaders import PyPDFLoader
+
+class LoadFile:
+    def __init__(self, doc):
+        self.docs = doc
+
+    def load_pdf(self):
+        pdf = self.docs
+
+        loader = [PyPDFLoader(pdf)]
+        base = []
+        for doc in loader:
+            base.extend(doc.load())
+        return base
