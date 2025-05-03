f690baa27284ddef05a48ea6c056a85b430b6281
juanvieira <juanvieira1@yahoo.com>
2025-05-02 22:09:52 -0300

    chore: implement config module

---
+++ utils/config.py
@@ -0,0 +1,18 @@
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+class LoadEnvVars:
+    def __init__(self, key: str):
+        self.key = key
+
+    def get_key(self) -> str:
+        key = os.getenv(self.key)
+        if not key:
+            raise ValueError("""
+                Your environment variable was not found.
+                Please double check this step and try again.
+                """)
+        
+        return key
